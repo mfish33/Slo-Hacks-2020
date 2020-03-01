f5ed2e94df560e20a5cd4e7641bd0d6dfdddecf2
kaitrias <kaitrias@gmail.com>
2020-02-29 20:55:51 -0800

    Add tax information

--- src/app/basic-info/basic-info.component.ts
+++ src/app/basic-info/basic-info.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 import {DataStoreService} from '../services/data-store.service';
-import {debounceTime, take, debounce} from 'rxjs/operators';
+import {debounceTime, take, debounce, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-basic-info',
   templateUrl: './basic-info.component.html',
-  styles: [':host{margin:20px;}','  #container{display: flex;justify-content: space-between;}',' .spread{width: 26%}']
+  styleUrls: ['./basic-info.component.css']
 })
 export class BasicInfoComponent implements OnInit {
 
@@ -67,6 +67,12 @@ export class BasicInfoComponent implements OnInit {
    WY : 'Wyoming'
   }
 
+  taxInfo = {
+      effectiveTaxRate: 0.0,
+      totalIncomeTax: 0.0,
+      incomeAfterTaxes: 0.0
+  };
+
   constructor(private fb: FormBuilder, private dataService: DataStoreService) {
     this.basicInfoForm = this.fb.group({
       sheetName:'',
@@ -77,15 +83,69 @@ export class BasicInfoComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    
-
     this.dataService.doc$.pipe(take(1)).subscribe(retreive => {
          this.basicInfoForm.patchValue(retreive.personalInfo);
       });
 
-    this.basicInfoForm.valueChanges.pipe(debounceTime(500))
+    this.basicInfoForm.valueChanges.pipe(
+      tap((change) => {
+            this.taxInfo.totalIncomeTax = this.calculateTaxes(change.income);
+            console.log(change.income / this.taxInfo.totalIncomeTax)
+            this.taxInfo.effectiveTaxRate = this.taxInfo.totalIncomeTax / change.income;
+            this.taxInfo.incomeAfterTaxes = change.income - this.taxInfo.totalIncomeTax;
+         }),
+      debounceTime(500))
       .subscribe(update =>{
          this.dataService.updateData({personalInfo: update});
       });
+   }
+
+   calculateTaxes(income) {
+      let total = 0.0;
+      if (income > 510300) {
+         total  = (income - 510300) * .37 +
+                  (510300 - 204100) * .35 +
+                  (204100 - 160725) * .32 +
+                  (160725 - 84200) * .24 +
+                  (84200 - 39475) * .22 +
+                  (39475 - 9700) * .12 +
+                  9700 * .1;
+      } else if (income > 204100) {
+         total  = (income - 204100) * .35 +
+                  (204100 - 160725) * .32 +
+                  (160725 - 84200) * .24 +
+                  (84200 - 39475) * .22 +
+                  (39475 - 9700) * .12 +
+                  9700 * .1;
+      } else if (income > 160725) {
+         total = (income - 160726) * .32 +
+                  (160725 - 84200) * .24 +
+                  (84200 - 39475) * .22 +
+                  (39475 - 9700) * .12 +
+                  9700 * .1;
+      } else if (income > 84200) {
+         total = (income - 84200) * .24 +
+                  (84200 - 39475) * .22 +
+                  (39475 - 9700) * .12 +
+                  9700 * .1;
+      } else if (income > 39475) {
+         total = (income - 39475) * .22 +
+                  (39475 - 9700) * .12 +
+                  9700 * .1;
+      } else if (income > 9700) {
+         total = (income - 9700) * .12 +
+                  9700 * .1;
+      } else {
+         total = income * .1;
+      }
+      return total;
+   }
+
+   formatMoney(money) {
+      return money.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    }
+
+   get showTaxSection(): boolean {
+      return this.basicInfoForm.get('state').value !== '' && this.basicInfoForm.get('income').value !== 0;
    }
 }
